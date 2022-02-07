83475e20c89e04be50f384c2f7b19b061ca0e1d3
JakubStranianek <j.stranianek@gmail.com>
2022-02-07 17:58:59 +0100

    useState to setTitle of homepage

--- tunes/src/views/Homepage.tsx
+++ tunes/src/views/Homepage.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import "./Home.scss"
 import logo from "../assets/logo.svg"
 
-const Home = () => (
+
+const Home: React.FC = () => {
+  const [title] = useState("ReactWheee!!")
+  
+  return (
   <div className='overflow'>
-    <h2>Homepage</h2>
+    <h2>{title}</h2>
     <p>
      Welcome on React homepage!
       </p>
 
       <img src={logo} alt="logoReact" />
   </div>
-);
+  )
+}
 
 export default Home;
